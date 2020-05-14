2f67d3829e9ec426d62422fbdfded89b083861c6
Praveen Mishra <praveen.mishra2@cognizant.com>
2020-05-14 22:16:56 +0530

    Last Commit

---
+++ build/precache-manifest.bfa91520b26d62386ecc0c0ac7bfa4db.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "7b1c8f31f8b5870ea9fd",
+    "url": "/static/css/main.5de73015.chunk.css"
+  },
+  {
+    "revision": "7b1c8f31f8b5870ea9fd",
+    "url": "/static/js/main.7b1c8f31.chunk.js"
+  },
+  {
+    "revision": "4edfc30aab64a97e557d",
+    "url": "/static/js/1.4edfc30a.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "75d390e5eabc3254418e950df631389b",
+    "url": "/static/media/add.75d390e5.svg"
+  },
+  {
+    "revision": "20e8847d1eb7759c19b0e30beb8849cf",
+    "url": "/static/media/arrow-back.20e8847d.svg"
+  },
+  {
+    "revision": "9d4e3f366f039249695de85a30a02b95",
+    "url": "/static/media/arrow-drop-down.9d4e3f36.svg"
+  },
+  {
+    "revision": "553ef4ded46a16719ccaddffe2528246",
+    "url": "/index.html"
+  }
+];
